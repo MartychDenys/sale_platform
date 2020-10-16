c5987b863442edf591a4a86468b0af4ab2b9d388
denysmartych <martych88@gmail.com>
2020-10-16 14:11:31 +0300

    prepare to develop

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "port": process.env.PORT || 5000,
+    "port": process.env.PORT || 5555,
     "mongoUri": "mongodb://denys:7908064d@ds263295.mlab.com:63295/mean_db",
     "jwtSecret": "Denys&Max",
     "baseUrl": "http://localhost:5000"
